b4a81b33fc4c7dedabd9c07919976b32d49c5b1a
TongYuZhe <1334933655@qq.com>
2019-06-03 17:54:37 +0800

    插入排序箭头指示动画

--- html/js/alg/insertSort/index.js
+++ html/js/alg/insertSort/index.js
@@ -15,7 +15,7 @@
 
 var speed = 0.5;
 var next = 0;
-var arr=[3,44,38,5,47,15,36,26,27,2,14,16];
+var arr=[3,44,38,5];
 insertCreate(arr.length);
 
 for(let i=0;i<arr.length;i++){
@@ -25,30 +25,50 @@ for(let i=0;i<arr.length;i++){
 
 async function insertionSort(array) {
     console.time('插入排序耗时');
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length-1; i++) {
         let j = i;
-        while(arr[j+1] < arr[j]){
+        while (arr[j] > arr[j+1]) {
             let stop = $("#insertStop").val();
-            if(stop == "已暂停" && next == 0 || speed == 0){
-                console.log("暂停",next,speed);
+            if(stop == "已暂停" && next == 0 || speed == 0) {
+                console.log("暂停", next, speed);
                 let result = await sleep(200);
-            }else {
-                if(next > 0){
+            } else {
+                if (next > 0) {
                     next--;
                 }
                 let da = arr[j + 1];
                 arr[j + 1] = arr[j];
-                dv(j);
+                await dv(j,1);
                 arr[j] = da;
                 j--;
-                let result = await sleep(speed*1000);
+                let result = await sleep(speed * 1000);
                 //dv(arr);
                 // $(".li"+j).css({"background":"#000"});
                 // $(".li"+(j+1)).css({"background":"#000"});
             }
         }
-        $("#xx").append("<li>第"+i+"次排序结果"+arr+"</li>")
+        while (arr[j] <= arr[j+1]) {
+            let stop = $("#insertStop").val();
+            if(stop == "已暂停" && next == 0 || speed == 0) {
+                console.log("暂停", next, speed);
+                let result = await sleep(200);
+            } else {
+                if (next > 0) {
+                    next--;
+                }
+
+                await dv(j,0);
+                j--;
+                let result = await sleep(speed * 1000);
+                //dv(arr);
+                // $(".li"+j).css({"background":"#000"});
+                // $(".li"+(j+1)).css({"background":"#000"});
+            }
+        }
+        $("#insertWrap .p3").css({"width":(i+1)*60+"px"});
+        $("#xx").append("<li>第"+(i+1)+"次排序结果"+arr+"</li>")
     }
+    $("#insertWrap .p3").css({"width":(arr.length)*60+"px"});
     console.timeEnd('插入排序耗时');
     $(".insertText").text("排序结束");
     return array;
@@ -56,7 +76,7 @@ async function insertionSort(array) {
 
 insertionSort(arr);
 
-$("#insertStop").click(function() { //文本框失去焦点后
+$("#insertStop").click(function() {
     if($("#insertStop").val() == "运行中"){
         $("#insertStop").val("已暂停");
         $("#insertStop").css({"background":"red"});
@@ -67,6 +87,6 @@ $("#insertStop").click(function() { //文本框失去焦点后
         //$(".insertText").text("运行中...");
     }
 })
-$("#insertNext").click(function() { //文本框失去焦点后
+$("#insertNext").click(function() {
     next++;
 })
--- html/js/alg/insertSort/insertSort.js
+++ html/js/alg/insertSort/insertSort.js
@@ -13,13 +13,27 @@ function sleep(second) {
         }, second);
     })
 }
-function dv(j){
-    /*两个div交换left位置*/
-    $(".li"+(j+1)).css({"left":$(".li"+j).css("left")});
-    $(".li"+(j)).css({"left":$(".li"+(j+1)).css("left")});
+async function dv(j,jj){
+    console.log(j,j+1,jj);
+    $("#insertWrap .p1").css({"left":((j+0)*40+(j*20))+7+"px"});
+    $("#insertWrap .p2").css({"left":((j+1)*40+((j+1)*20))+24+"px"});
+    await sleep(speed*1000);
+    $(".li"+(j+1)).css({"background":"red"});
+    $(".li"+(j)).css({"background":"red"});
 
-    /*两个div交换class*/
-    $(".li"+j).addClass("lix").removeClass("li"+j);
-    $(".li"+(j+1)).addClass("li"+j).removeClass("li"+(j+1));
-    $(".lix").addClass("li"+(j+1)).removeClass("lix");
+    await sleep(speed*2000);
+    if(jj == 1) {
+        /*两个div交换left位置*/
+        $(".li" + (j + 1)).css({"left": $(".li" + j).css("left")});
+        $(".li" + (j)).css({"left": $(".li" + (j + 1)).css("left")});
+
+        /*两个div交换class*/
+        $(".li" + j).addClass("lix").removeClass("li" + j);
+        $(".li" + (j + 1)).addClass("li" + j).removeClass("li" + (j + 1));
+        $(".lix").addClass("li" + (j + 1)).removeClass("lix");
+    }
+    $(".li"+(j+1)).css({"background":"#00c4ff"});
+    $(".li"+(j)).css({"background":"#00c4ff"});
+
+
 }
